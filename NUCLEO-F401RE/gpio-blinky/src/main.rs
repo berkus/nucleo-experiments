4a8baa20b52bc5a61537c893bb580c6b26be7862
Berkus Decker <berkus+github@metta.systems>
2024-01-14 02:28:35 +0200

    chore(test): Try debounced-pin
    NB: doesn't work here.

--- NUCLEO-F401RE/gpio-blinky/src/main.rs
+++ NUCLEO-F401RE/gpio-blinky/src/main.rs
@@ -3,6 +3,7 @@
 
 use {
     cortex_m_rt::entry,
+    debounced_pin::{prelude::*, ActiveLow},
     defmt_rtt as _, panic_probe as _,
     stm32f4xx_hal::{
         gpio::Pin,
@@ -26,27 +27,34 @@ fn main() -> ! {
 
     let gpioc = dp.GPIOC.split();
     let button = gpioc.pc13;
+    let mut button = DebouncedInputPin::new(button, ActiveLow);
 
     let mut delay = 10_0000_u32;
 
     led.set_low();
 
     loop {
-        delay = loop_delay(delay, &button);
+        delay = loop_delay(delay, &mut button);
         led.toggle();
     }
 }
 
-fn loop_delay<const P: char, const N: u8>(mut delay: u32, button: &Pin<P, N>) -> u32 {
+fn loop_delay<const P: char, const N: u8>(
+    mut delay: u32,
+    button: &mut DebouncedInputPin<Pin<P, N>, ActiveLow>,
+) -> u32 {
     defmt::println!("Waiting for {}", delay);
     for _ in 1..delay {
-        if button.is_low() {
-            delay -= 2_5000_u32;
-            if delay < 2_5000_u32 {
-                delay = 10_0000_u32;
+        match button.update().expect("whatever") {
+            DebounceState::Active => {
+                delay -= 2_5000_u32;
+                if delay < 2_5000_u32 {
+                    delay = 10_0000_u32;
+                }
+                defmt::println!("delay: {}", delay);
+                return delay;
             }
-            defmt::println!("delay: {}", delay);
-            return delay;
+            _ => (),
         }
     }
     delay
